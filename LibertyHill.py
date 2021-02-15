51bee82e0de2990496bd6e08e504bfb4548463e5
Gabriel Mongaras <gabriel@mongaras.com>
2021-02-14 19:06:14 -0600

    Made Changes for GT

--- LibertyHill.py
+++ LibertyHill.py
@@ -5,7 +5,7 @@ import csv
 
 def main():
     # Open the webpage
-    page = requests.get('https://members.libertyhillchamber.org/list/search?q=0-9%2c+a&o=alpha')
+    page = requests.get('https://business.georgetownchamber.org/list/search?q=0-9%2c+a&o=alpha')
     soup = BeautifulSoup(page.content, 'html.parser')
 
     # Get all the items in the webpage which we will be using.
@@ -24,38 +24,60 @@ def main():
         except:
             companyName = item.find_all(class_='gz-card-title')[0].text.strip('\n')
 
+        # If the company is RPI Inc., hard code it
+        if companyName == "RPI, Inc":
+            writer.writerow(["RPI Inc.", "1880 S Dairy Ashford Rd", "Houston", "TX", "77077", "(128) 155-84444"])
+            continue
+        
         #Get the address and phone number. Make sure there is no missing data.
         streetAddress = item.find_all(class_='gz-street-address')
         streetAddress = check(streetAddress).strip('"')
         city = item.find_all(class_='gz-address-city')
         city = check(city)
-        if item.find_all(class_='gz-fal gz-fa-phone') != []:
-            state = item.find_all(class_=None)[-3]
-            state = check(state)
-            zipCode = item.find_all(class_=None)[-2]
-            zipCode = check(zipCode)
-            phone = item.find_all(class_=None)[-1]
-            phone = check(phone)
-        else:
-            a = item.find_all(itemprop='citystatezip')
-            state = item.find_all(class_=None)[-2]
-            state = check(state)
-            zipCode = item.find_all(class_=None)[-1]
-            zipCode = check(zipCode)
-            phone = "N/A"
-
-        # Since there is a pice of data that is missing from the website, I will
-        #write it in manually.
-        if companyName == "Schlotzsky's":
-            writer.writerow(['Simplified Engineering', '407 Forest Street', 'Liberty Hill', 'TX', '78642', '(512) 947-4141'])
-
+        
+        try:
+            czp = item.find(itemprop="citystatezip").text.split("\n")
+            for i in range(0, len(czp)):
+                if czp[i] != '':
+                    if ord(czp[i][-1]) == 32:
+                        czp[i] = czp[i][:-1]
+            if czp[2].upper() == "TEXAS" or czp[2].upper() == "TX" or czp[2] == "US":
+                state = czp[2]
+                zipCode = czp[3]
+            else:
+                state = czp[1]
+                zipCode = czp[2]
+            if item.find_all(class_='gz-fal gz-fa-phone') != []:
+                phone = item.find_all(class_=None)[-1]
+            else:
+                phone = "N/A"
+        except:
+            if item.find_all(class_='gz-fal gz-fa-phone') != []:
+                if item.find_all(class_="gz-street-address") != []:
+                    state = item.find_all(class_=None)[-3]
+                    zipCode = item.find_all(class_=None)[-2]
+                    phone = item.find_all(class_=None)[-1]
+                else:
+                    state = ""
+                    zipCode = ""
+                    phone = item.find_all(class_=None)[-1]
+            else:
+                state = item.find_all(class_=None)[-2]
+                zipCode = item.find_all(class_=None)[-1]
+                phone = "N/A"
+        state = check(state)
+        zipCode = check(zipCode)
+        phone = check(phone)
+        
         #Write the data to the csv file.
         writer.writerow([companyName, streetAddress, city, state, zipCode, phone])
 
 
 def check(dataPoint):
-    if dataPoint is None or dataPoint == []:
+    if dataPoint is None or dataPoint == [] or dataPoint == "":
         dataPoint = 'N/A'
+    elif isinstance(dataPoint, str):
+        return dataPoint
     elif isinstance(dataPoint, list):
         dataPoint = dataPoint[0].text
     else:
